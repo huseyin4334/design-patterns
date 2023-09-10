45c7671b1985700cac7ac80fef5e89498260855d
26075232 <huseyincan_kalcik@arcelik.com>
2023-09-10 22:54:31 +0300

    observer design pattern concept and examples

--- observer/src/main/java/com/patterns/observer/observerandobservable/App.java
+++ observer/src/main/java/com/patterns/observer/observerandobservable/App.java
@@ -22,5 +22,7 @@ public class App
 
     /*
         An observer is an object that wishes to be informed about events happening in the system. The entity generating the events is an observable.
+
+        More info: https://refactoring.guru/design-patterns/observer
      */
 }
